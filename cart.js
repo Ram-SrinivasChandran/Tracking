0489558d429ca80d740f6ccc1619b6c42747acf9
Ram-SrinivasChandran <83490721+Ram-SrinivasChandran@users.noreply.github.com>
2023-06-29 12:52:33 +0530

    Add files via upload

---
+++ cart.js
@@ -0,0 +1,4 @@
+const cartProducts=JSON.parse(localStorage.getItem("cartProducts"))||[];
+/*document.querySelector('.js-button').addEventListener('click',()=>{
+  localStorage.removeItem("cartProducts");
+})*/
---
+++ trackDisplay.js
@@ -0,0 +1,23 @@
+displayCartCount();
+if(tracks.length!==0){
+  let html;
+tracks.forEach((track)=>{
+html=`<p class="view-order">
+<a href="https://ram-srinivaschandran.github.io/Orders/orders.html">View all orders</a></p>
+<div class="arriving">Arriving on ${track.productDay}, ${track.productMonth} ${track.productDate}</div>
+<div class="product-name">${track.productName}</div>
+<div class="quantity">Quantity: ${track.productQuantity}</div>
+<div class="product-image-section"><img class="product-image" src="${track.productImage}"></div>
+<div class="delivery">
+<div>Preparing</div>
+<div class="ship">Shipping</div>
+<div>Delivered</div>
+</div>
+<div class="delivery-status">
+<div class="processing"></div>
+</div>`
+});
+//console.log(html);
+document.querySelector('.js-track').innerHTML=html;
+localStorage.removeItem("tracks");
+}
