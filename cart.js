69a542c62cdd566bd2b8aefa5d6782fc2516f847
Ram-SrinivasChandran <83490721+Ram-SrinivasChandran@users.noreply.github.com>
2023-06-29 14:44:49 +0530

    Delete cart.js

--- cart.js
+++
@@ -1,4 +0,0 @@
-const cartProducts=JSON.parse(localStorage.getItem("cartProducts"))||[];
-/*document.querySelector('.js-button').addEventListener('click',()=>{
-  localStorage.removeItem("cartProducts");
-})*/
